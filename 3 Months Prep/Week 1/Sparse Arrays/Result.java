3450dec155353f31f2f58364a54e798d6ce388a1
Shaun Yuen <91139027+shaunyuencw@users.noreply.github.com>
2021-10-07 00:26:31 +0800

    Create Result.java
    Completed Week 1 Problem 7

---
+++ 3 Months Prep/Week 1/Sparse Arrays/Result.java
@@ -0,0 +1,93 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'matchingStrings' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. STRING_ARRAY strings
+     *  2. STRING_ARRAY queries
+     */
+
+    public static List<Integer> matchingStrings(List<String> strings, List<String> queries) {
+    // Write your code here
+        HashMap<String, Integer> dict = new HashMap<String, Integer>();
+        ArrayList<Integer> matches = new ArrayList<Integer>();
+        
+        // Traverse strings once
+        for (int i = 0; i < strings.size(); i++){
+            if (dict.containsKey(strings.get(i))){
+                dict.put(strings.get(i), dict.get(strings.get(i)) + 1);
+            }
+            else{
+                dict.put(strings.get(i), 1);
+            }
+        }
+        
+        // Traverse queries
+        for (int j = 0; j < queries.size(); j++){
+            if (dict.containsKey(queries.get(j))){
+                matches.add(dict.get(queries.get(j)));
+            }
+            else{
+                matches.add(0);
+            }
+        }
+        
+        return matches;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int stringsCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<String> strings = IntStream.range(0, stringsCount).mapToObj(i -> {
+            try {
+                return bufferedReader.readLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        })
+            .collect(toList());
+
+        int queriesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<String> queries = IntStream.range(0, queriesCount).mapToObj(i -> {
+            try {
+                return bufferedReader.readLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        })
+            .collect(toList());
+
+        List<Integer> res = Result.matchingStrings(strings, queries);
+
+        bufferedWriter.write(
+            res.stream()
+                .map(Object::toString)
+                .collect(joining("\n"))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
